ee877bdaef4298f7502975f8f0374846a8eeb03c
Wilda <Wilda@DESKTOP-9P4GRN0>
2018-12-13 13:08:54 +0700

    Menambah Validasi disetiap Input pada Ticket Class

--- src/atm/Ticket.java
+++ src/atm/Ticket.java
@@ -14,27 +14,26 @@ import java.time.format.DateTimeFormatter;
  */
 public class Ticket extends Transaction {
     private final Keypad keypad;
+    private String pil;
+    
     public Ticket(int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase,Keypad atmKeypad) {
         super(userAccountNumber, atmScreen, atmBankDatabase);
         keypad = atmKeypad;
     }
-
 
     @Override
     public void execute() {
         Screen screen = getScreen();
         screen.displayMessage("\n1. Tiket Pesawat");
         screen.displayMessage("\n2. Tiket Kereta Api");
-        screen.displayMessage("\nPilihan : ");
-        int pilihan = keypad.getInput(); 
-        if(pilihan == 1){
+        pil = getPilihan();
+        
+        if(pil.matches("1")){
             screen.displayMessage("\n1. Garuda Indonesia");
             screen.displayMessage("\n2. Lion Air");
-            screen.displayMessage("\nPilihan : ");
-            int pil = keypad.getInput(); 
-            if(pil == 1 || pil == 2){
-                screen.displayMessage("\nMasukkan Nomor kode pemesanan anda : ");
-                int Kd =  keypad.getInput();
+            pil = getPilihan();
+            if(pil.matches("1") || pil.matches("2")){
+                int Kd =  getKdPembayaran();
                 if(getBankDatabase().validasiKdPesawat(Kd)){
                     double amount = super.getBankDatabase().getHargaPembayaranPesawat(Kd);
                     int booking = super.getBankDatabase().getKodeBookingPesawat(Kd);
@@ -46,7 +45,7 @@ public class Ticket extends Transaction {
                         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
                         LocalDateTime now = LocalDateTime.now();
                         super.getBankDatabase().debit(getAccountNumber(),amount); // Balance akun saya berkurang
-                        if (pil==1){
+                        if (pil.matches("1")){
                             super.getBankDatabase().credit(34567,amount);
                             super.getBankDatabase().tulisHistory(super.getAccountNumber(),"Membayar tiket pesawat Garuda Indonesia seharga $" + amount 
        + " " + dtf.format(now));
@@ -67,9 +66,8 @@ public class Ticket extends Transaction {
                     }else screen.displayMessageLine("Balance tidak cukup");      
                 }else screen.displayMessageLine("Kode tiket pesawat tidak tidak valid");
             }else screen.displayMessageLine("Nomor tidak tidak valid");
-        }else if (pilihan == 2){
-            screen.displayMessage("\nMasukkan Nomor kode pemesanan anda : ");
-            int Kd =  keypad.getInput();
+        }else if (pil.matches("2")){
+            int Kd =  getKdPembayaran();
             if(getBankDatabase().validasiKdKereta(Kd)){
                 double amount = super.getBankDatabase().getHargaPembayaran(Kd);
                 int booking = super.getBankDatabase().getKodeBooking(Kd);
@@ -90,6 +88,35 @@ public class Ticket extends Transaction {
             }else screen.displayMessageLine("Kode Pembayaran anda tidak tidak valid");
         }else screen.displayMessage("\nPilihan Anda tidak valid");
           
+    }
+    
+    private String getPilihan(){
+        String pilihan;
+        screen.displayMessage("\nPilihan : ");
+        pilihan = keypad.getString();
+        while((pilihan.length()>1) || (!pilihan.matches("[0-9]*"))){
+              if ((pilihan.length()>1) || (!pilihan.matches("[0-9]*"))){
+                 super.getScreen().displayMessage("Input Tidak Valid...");
+                }
+                screen.displayMessage("\nPilihan : ");
+                pilihan = keypad.getString();
+        }
+        return pilihan;
+    }
+    
+    private int getKdPembayaran(){
+        screen.displayMessage("\nMasukkan Nomor kode pembayaran anda : ");
+        String Kd =  keypad.getString();
+
+        while((Kd.length()>13) || (!Kd.matches("[0-9]*"))){
+              if ((Kd.length()>13) || (!Kd.matches("[0-9]*"))){
+                 super.getScreen().displayMessage("Input Tidak Valid...");
+                }
+                screen.displayMessage("\nMasukkan Nomor kode pembayaran anda : ");
+                Kd =  keypad.getString();
+        }
+        int Kode = Integer.parseInt(Kd);
+        return Kode;
     }
     
 }
