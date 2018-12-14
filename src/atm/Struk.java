3d18887f20dec3db4ab14026af8e538018f68b35
jaka0839 <jaka.ahmad.tif17@polban.ac.id>
2018-12-15 06:00:00 +0700

    Menambah struk

--- src/atm/Struk.java
+++ src/atm/Struk.java
@@ -17,17 +17,37 @@ public class Struk {
     private final int WITHDRAWAL = 1;
     private final int TRANSFER = 3;
     private final int DEPOSIT = 2;
+    private final int BAYARUKT = 4;
+    private final int PULSE = 5;
+    private final int ETOKEN = 6;
+    private final int ZFITRAH = 7;
+    private final int VA = 8;
     public Struk (double Amount, int userAccountNumber)
     {
         this.amount = Amount;
         this.account = userAccountNumber;
         
     }
+    public Struk (int userAccountNumber)
+    {
+        this.account = userAccountNumber;
+    }
     private void bar()
     {
         System.out.println("        Struk Transaksi              ");
         System.out.println("=====================================");
     }
+    
+    private void akun()
+    {
+        System.out.println("Akun: " + account);
+    }
+    
+    private void thx()
+    {
+        System.out.println("Terima kasih telah berlayanan");
+    }
+    
         
     public void CetakStruk (int strukFor, int receiver)
     {
@@ -40,14 +60,14 @@ public class Struk {
                 System.out.println("Akun: " + account);
                 System.out.println("Anda telah menarik uang sebesar: $" + amount);
                 System.out.println("pada waktu " + dtf.format(now));
-                System.out.println("   Terima kasih telah berlayanan");
+                System.out.println("Terima kasih telah berlayanan");
                 break;
             case DEPOSIT:
                 bar();
                 System.out.println("Akun: " + account);
                 System.out.println("Anda telah menyimpan uang sebesar: $" + amount);
                 System.out.println("pada waktu " + dtf.format(now));
-                System.out.println("   Terima kasih telah berlayanan");
+                System.out.println("Terima kasih telah berlayanan");
                 break;
             case TRANSFER:
                 bar();
@@ -57,8 +77,59 @@ public class Struk {
                 System.out.println("pada waktu: " + dtf.format(now));
                 System.out.println("Terima kasih telah berlayanan");
                 break;
+            case BAYARUKT:
+                bar();
+                System.out.println("Akun: " + account);
+                System.out.println("Anda telah membayar UKT sebesar: $" + amount);
+                System.out.println("Ke akun: " + receiver);
+                System.out.println("pada waktu: " + dtf.format(now));
+                System.out.println("Terima kasih telah berlayanan");
+                break;
+            case PULSE:
+                bar();
+                System.out.println("Akun: " + account);
+                System.out.println("Anda telah membeli pulsa dengan nominal: " + amount);
+                System.out.println("pada waktu: " + dtf.format(now));
+                System.out.println("Terima kasih telah berlayanan");
+                break;
+            case ETOKEN:
+                bar();
+                akun();
+                System.out.println("Anda telah membeli token seharga: " + amount);
+                System.out.println("pada waktu: " + dtf.format(now));
+                thx();
+                break;
+            case ZFITRAH:
+                bar();
+                akun();
+                System.out.println("Anda Telah membayar Zakat Fitrah");
+                thx();
+                break;
+            case VA:
+                bar();
+                break;
         }
     }
     
+    public void PrintBioskop(int jumlah , String title)
+    {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        bar();
+        akun();
+        System.out.println("Anda telah membeli tiket " + title + "seharga: " + amount);
+        System.out.println("Dengan jumlah: " + jumlah + " buah tiket");
+        System.out.println("pada waktu: " + dtf.format(now));
+        thx();
+    }
+    
+    public void PrintZMal()
+            
+    {
+        bar();
+        akun();
+        System.out.println("Anda telah berhasil membayar zakat mal");
+        thx();
+    }
         
 }
