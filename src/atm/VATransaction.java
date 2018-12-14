76bc0c9d47e4a64f4f1f08274e2a5162907a3dda
jaka0839 <jaka.ahmad.tif17@polban.ac.id>
2018-12-15 06:04:49 +0700

    Menambah struk di VATransaction

--- src/atm/VATransaction.java
+++ src/atm/VATransaction.java
@@ -36,9 +36,11 @@ public class VATransaction extends Transaction{
                 getBankDatabase().credit(getBankDatabase().getAccount(getAccountNumber()).getVA(code).getAccountReceiver(), 
                     getBankDatabase().getAccount(getAccountNumber()).getVA(code).getBill());
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
-       LocalDateTime now = LocalDateTime.now();
-       super.getBankDatabase().tulisHistory(super.getAccountNumber(),"Membayar transaksi virtual akun sebesar $" + getBankDatabase().getAccount(getAccountNumber()).getVA(code).getBill()
+                LocalDateTime now = LocalDateTime.now();
+                super.getBankDatabase().tulisHistory(super.getAccountNumber(),"Membayar transaksi virtual akun sebesar $" + getBankDatabase().getAccount(getAccountNumber()).getVA(code).getBill()
        + " " + dtf.format(now));
+                Struk struk = new Struk(getBankDatabase().getAccount(getAccountNumber()).getVA(code).getBill(),super.getAccountNumber());
+                struk.CetakStruk(8,0);
             }else screen.displayMessageLine("Insufficient fund");
         }else screen.displayMessageLine("You don't have that bill");
     }
